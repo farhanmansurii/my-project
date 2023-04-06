4484bbaefa5cf2703439189f39f3c720bab97908
Farhan <mansurifarhanfm@gmail.com>
2023-04-07 03:36:11 +0530

    fixed

--- src/components/Artplayer.jsx
+++ src/components/Artplayer.jsx
@@ -23,12 +23,14 @@ export default function EnimePlayer(props) {
           menu: [
             {
               name: "Next",
-              icon: `  <svg fill="none" viewBox="0 0 15 15" height="1em" width="1em" className="hover:scale-110">
-      <path
-        fill="currentColor"
-        d="M1.79 2.093A.5.5 0 001 2.5v10a.5.5 0 00.79.407l7-5a.5.5 0 000-.814l-7-5zM13 13h1V2h-1v11z"
-      />
-    </svg>
+              icon: ` <svg fill="white" className='w-6 h-6 '  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 512 512" xml:space="preserve">
+<path d="M256,0C114.615,0,0,114.615,0,256s114.615,256,256,256s256-114.615,256-256S397.385,0,256,0z M280.875,269.313l-96,64
+	C182.199,335.094,179.102,336,176,336c-2.59,0-5.184-0.625-7.551-1.891C163.246,331.32,160,325.898,160,320V192
+	c0-5.898,3.246-11.32,8.449-14.109c5.203-2.773,11.516-2.484,16.426,0.797l96,64C285.328,245.656,288,250.648,288,256
+	S285.328,266.344,280.875,269.313z M368,320c0,8.836-7.164,16-16,16h-16c-8.836,0-16-7.164-16-16V192c0-8.836,7.164-16,16-16h16
+	c8.836,0,16,7.164,16,16V320z"/>
+</svg>
 `,
               onClick: () => {
                 deets?.type === "TV Series" ?
@@ -50,8 +52,9 @@ export default function EnimePlayer(props) {
         title:
           deets?.type === "Movie" ?
             (`${deets?.title}`) :
-            (` S${selectedEpisode?.season}E${selectedEpisode?.season}  ${selectedEpisode?.title}`),
+            (` S${selectedEpisode?.season}E${selectedEpisode?.episode}  ${selectedEpisode?.title}`),
         src: source,
+        poster: selectedEpisode?.img.hd || selectedEpisode?.img.mobile || deets.image
       });
     }
     if (subtitles) {
@@ -61,7 +64,7 @@ export default function EnimePlayer(props) {
 
   return (
     <div>
-      <div className="w-full h-full p-0 m-0" ref={playerContainerRef} />
+      <div className="w-full  aspect-video p-0 m-0" ref={playerContainerRef} />
     </div>
   );
 }
