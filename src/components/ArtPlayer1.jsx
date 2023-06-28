a3e2a81a38199591023d0c968049ef04c2d7665f
Farhan Mansuri <mansurifarhanfm@gmail.com>
2023-06-29 04:28:40 +0530

    fixed

--- src/components/ArtPlayer1.jsx
+++ src/components/ArtPlayer1.jsx
@@ -21,11 +21,11 @@ const Enime1Player = ({ episode, getNextEpisode, deets, selectedEpisode }) => {
       } else
       {
         console.log(deets.seasons[seasonIndex + 1].episodes[0])
-        return "Start S" + deets.seasons[seasonIndex + 1].episodes[0].season
+        return "Start Season " + deets.seasons[seasonIndex + 1].episodes[0].season
       }
     } else
     {
-      return "Play E" + deets.seasons[seasonIndex].episodes[episodeIndex + 1].episode;
+      return "Play Episode " + deets.seasons[seasonIndex].episodes[episodeIndex + 1].episode;
     }
 
   }
@@ -49,7 +49,7 @@ const Enime1Player = ({ episode, getNextEpisode, deets, selectedEpisode }) => {
       playerRef.current = Player.make('#oplayer', { poster, title }).use([ui({
         theme: {
           primaryColor: "red"
-        },
+        }, preload: 'auto',
         subtitle: {
           fontSize: 14,
           background: true,
@@ -84,7 +84,7 @@ const Enime1Player = ({ episode, getNextEpisode, deets, selectedEpisode }) => {
           deets?.type === "TV Series" ?
             {
               name: getNextEp(selectedEpisode, deets),
-              position: 'top',
+
 
               onClick: () => {
                 if (deets?.type === "TV Series")
@@ -100,9 +100,7 @@ const Enime1Player = ({ episode, getNextEpisode, deets, selectedEpisode }) => {
               position: 'top',
 
             },
-        ], icons: {
-          fullscreen: [`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-maximize-2"><polyline points="15 3 21 3 21 9"/><polyline points="9 21 3 21 3 15"/><line x1="21" x2="14" y1="3" y2="10"/><line x1="3" x2="10" y1="21" y2="14"/></svg>`, `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-minimize-2"><polyline points="4 14 10 14 10 20"/><polyline points="20 10 14 10 14 4"/><line x1="14" x2="21" y1="10" y2="3"/><line x1="3" x2="10" y1="21" y2="14"/></svg>`]
-        }
+        ], 
       }), hls({ forceHLS: true }),]).create();
 
     }
